88b5e710205a8ded7f313fce337f58ae5aced6f6
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-07-21 18:58:02 +0100

    feat(schema): export track-b canonical contracts

---
+++ lib/property-schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./core";
+export * from "./project-schema-v1";
+export * from "./completeness";
+export * from "./onboarding";
+export * from "./enrichment";
+export * from "./validation";
+export * from "./adapters";
