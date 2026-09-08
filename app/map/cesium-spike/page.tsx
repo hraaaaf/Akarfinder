afbbd19657e8a5681bb40b6f7cd9b0cbcc1011dc
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-08 14:00:26 +0100

    feat(vivre-ici): activate target lens on Cesium spike

--- app/map/cesium-spike/page.tsx
+++ app/map/cesium-spike/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { CesiumMaarifSpike } from "@/components/map/CesiumMaarifSpike";
+import { CesiumTargetLens } from "@/components/map/CesiumTargetLens";
 import { MaarifTargetRail } from "@/components/map/MaarifTargetRail";
 
 export const metadata: Metadata = {
@@ -12,6 +13,7 @@ export default function CesiumSpikePage() {
   return (
     <main className="relative min-h-[100svh] bg-[#f4efe7]" data-vivre-ici-cesium-spike-page>
       <SiteHeader searchMode fluid />
+      <CesiumTargetLens />
       <CesiumMaarifSpike />
       <MaarifTargetRail />
     </main>
