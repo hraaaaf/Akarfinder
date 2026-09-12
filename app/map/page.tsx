f9e649c0edad2c167f85553192b4df72cd8c847b
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-12 08:59:33 +0100

    style(vivre-ici): finish clinical mobile map chrome

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -14,6 +14,7 @@ import "./premium-lot6.css";
 import "./premium-lot7.css";
 import "./premium-lot8.css";
 import "./target-alignment.css";
+import "./clinical-final.css";
 
 export const dynamic = "force-dynamic";
 
